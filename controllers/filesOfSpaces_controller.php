7e93addabe750f7a524b502cdaf3bc59c1c4d317
Mateos Puerta, Sergio <smatpue573@g.educaand.es>
2023-03-02 17:57:21 +0100

    Ligeros cambios en el sistema de logueo

--- controllers/filesOfSpaces_controller.php
+++ controllers/filesOfSpaces_controller.php
@@ -6,6 +6,14 @@
     require_once("../models/filesOfSpaces_model.php");
     require_once("../db/conexion.php");
 
+    $path = "../Spaces/";
+    if ($handler = opendir($thefolder)) {
+        while (false !== ($file = readdir($handler))) {
+            echo "$file<br>";
+        }
+        closedir($handler);
+    }
+
 
     //Llamada a la vista
     require_once("../views/filesOfSpaces_view.php");
--- views/filesOfSpaces_view.php
+++ views/filesOfSpaces_view.php
@@ -72,7 +72,36 @@
     </nav>
     <!------------------------------- ESPACIOS ------------------------------>
     
-    
+    <div class="container-fluid">
+  <div class="px-lg-5">
+
+    <!--------------------- CABECERA --------------------->
+    <div class="row py-5">
+      <div class="col-lg-12 mx-auto rounded bg-light">
+        <h1 class="display-4"><?= $space_name ?></h1>
+        <p class="lead">Fecha de creación: </p>
+      </div>
+    </div>
+    <!---------------------------------------------------->
+
+    <div class="row">
+      <!-- Gallery item -->
+      <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+        <div class="bg-white rounded shadow-sm"><img src="https://bootstrapious.com/i/snippets/sn-gallery/img-1.jpg" alt="" class="img-fluid card-img-top">
+          <div class="p-4">
+            <h5> <a href="#" class="text-dark">Red paint cup</a></h5>
+            <p class="small text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+            <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
+              <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">JPG</span></p>
+              <div class="badge badge-danger px-3 rounded-pill font-weight-normal">New</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End -->
+    </div>
+  </div>
+</div>
     <!-------------------------- BOTON NUEVO ESPACIO --------------------------->
     <button type="button" class="btn btn-flotante" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Subir archivo</button>
 
